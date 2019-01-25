f66e828496cfc03b894372acb46ea0c5fe6a8e32
Joe Bain <mrjoebain@gmail.com>
2019-01-25 13:49:53 +0000

    first commit. wwise setup

---
+++ Assets/Sounder.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sounder : MonoBehaviour
+{
+    public void PlaySFX01()
+    {
+        AkSoundEngine.PostEvent("Play_SFX_01", gameObject);
+    }
+
+    public void PlaySFX02()
+    {
+        AkSoundEngine.PostEvent("Play_SFX_02", gameObject);
+    }
+
+    public void PlaySFX03()
+    {
+        AkSoundEngine.PostEvent("Play_SFX_03", gameObject);
+    }
+
+    public void PlaySFX04()
+    {
+        AkSoundEngine.PostEvent("Play_SFX_04", gameObject);
+    }
+
+    public void PlaySFX05()
+    {
+        AkSoundEngine.PostEvent("Play_SFX_05", gameObject);
+    }
+
+    public void PlayMusic01()
+    {
+        AkSoundEngine.PostEvent("Play_Music_01", gameObject);
+    }
+
+    public void PlayMusic02()
+    {
+        AkSoundEngine.PostEvent("Play_Music_02", gameObject);
+    }
+
+    public void PauseMusic()
+    {
+        AkSoundEngine.PostEvent("Pause_Music", gameObject);
+    }
+
+    public void UnpauseMusic()
+    {
+        AkSoundEngine.PostEvent("Unpause_Music", gameObject);
+    }
+}
---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,49 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PAUSE_MUSIC = 2735935537U;
+        static const AkUniqueID PLAY_MUSIC_01 = 3709355747U;
+        static const AkUniqueID PLAY_MUSIC_02 = 3709355744U;
+        static const AkUniqueID PLAY_SFX_01 = 3257337847U;
+        static const AkUniqueID PLAY_SFX_02 = 3257337844U;
+        static const AkUniqueID PLAY_SFX_03 = 3257337845U;
+        static const AkUniqueID PLAY_SFX_04 = 3257337842U;
+        static const AkUniqueID PLAY_SFX_05 = 3257337843U;
+        static const AkUniqueID UNPAUSE_MUSIC = 3712621630U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SFX = 393239870U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
