c1d3d9edaea96cbb06fe76582080f329f052e305
Joe Bain <mrjoebain@gmail.com>
2019-01-26 13:47:50 +0000

    pipe gen using prefab sections

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -3,44 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-[Serializable]
-public class Rule
-{
-    public MapGenerator.PipeSectionTypes From;
-    public MapGenerator.PipeSectionTypes[] To;
-}
-
 public class MapGenerator : MonoBehaviour
 {
     public Tilemap tilemap;
     public int SectionCount = 100;
-    public int SectionLength = 8;
 
-    public TileBase TopPipeWall, BottomPipeWall, Water, Earth;
+    public PipeSection[] SectionPrefabs;
 
-    public Rule[] Rules;
-    Dictionary<PipeSectionTypes, Rule> ruleDictionary;
+    private List<PipeSection> pipeSections;
+    private Dictionary<PipeSection.JointType, List<PipeSection>> sectionDictionary;
 
     void Start()
     {
         DoGenerateMap();
     }
-
-    public enum PipeSectionTypes
-    {
-        Small,
-        Medium,
-        Big,
-        Split,
-        SmallGoingUp,
-        SmallGoingDown,
-        MediumGoingUp,
-        MediumGoingDown,
-        Transition
-    }
-
-    List<PipeSectionTypes> pipeSections = new List<PipeSectionTypes>();
-
+    
     [InspectorButton("DoGenerateMap")]
     public bool _doGenerateMap;
 
@@ -48,24 +25,8 @@ public class MapGenerator : MonoBehaviour
     {
         GenerateMap(UnityEngine.Random.Range(1, 100000));
     }
-    /// <summary>
-    /// When generating a map we want it to mainly go left to right, we want to vary the width between narrow, medium, and wide, we want to
-    /// have some sloped sections, and the camera will track up and down accordingly. For now we won't worry about vertical sections or right to
-    /// left sections.
-    /// 
-    /// It would be nice to be able to introduce feature sections, such as split tunnels which rejoin, gaps in the pipe, that you have to jump,
-    /// waterfalls maybe, cross sections which might have enemies or currents crossing vertically.
-    /// 
-    /// We then need another layer which will place enemies, and another layer which will place scenery like plants, rocks, etc. Some enemies or
-    /// obstacles could require changes to the background, very large enemies coming out of a side pipe, or plants bursting through a crack
-    /// in the pipe.
-    /// 
-    /// It would be nice to have another layer for parallax objects in the background and foreground.
-    /// 
-    /// Probably another layer for currents in the pipe too, which might not be visible in the typical way, but would spawn some particle effect
-    /// or mean some certain animation is played on the background tiles in that spot.
-    /// </summary>
-    /// <param name="seed"></param>
+    
+
     public void GenerateMap(int seed)
     {
         FillRuleDictionary();
@@ -74,96 +35,50 @@ public class MapGenerator : MonoBehaviour
         GenerateSections(seed);
         GenerateTiles();
     }
-
+    
     private void GenerateSections(int seed)
     {
         var rng = new System.Random(seed);
-        pipeSections = new List<PipeSectionTypes>(SectionCount);
-        PipeSectionTypes currentSection = PipeSectionTypes.Medium, nextSection;
+
+        pipeSections = new List<PipeSection>();
+
+        PipeSection prevSection = null, nextSection;
         for (int s = 0; s < SectionCount; s++)
         {
-            Rule rule = ruleDictionary[currentSection];
-            nextSection = rule.To[rng.Next(rule.To.Length)];
+            List<PipeSection> possibleSections = prevSection == null ? new List<PipeSection>(SectionPrefabs) : sectionDictionary[prevSection.EndJoint];
+            nextSection = possibleSections[rng.Next(possibleSections.Count)];
             pipeSections.Add(nextSection);
-            if (s > 0 && pipeSections[s - 1] != pipeSections[s] && pipeSections[s - 1] != PipeSectionTypes.Transition)
-            {
-                if (s >= SectionCount - 1)
-                {
-                    pipeSections[s] = pipeSections[s - 1];
-                    break;
-                }
-                pipeSections.Add(pipeSections[s]);
-                pipeSections[s] = PipeSectionTypes.Transition;
-                s++;
-            }
-            currentSection = nextSection;
+            prevSection = nextSection;
         }
     }
 
     private void GenerateTiles()
     {
-        Vector3Int centre = new Vector3Int(0, 0, 0);
-
-        int t = 0;
-        PipeSectionTypes section = pipeSections[0], prevSection = pipeSections[0], nextSection = pipeSections[1];
+        Vector3Int cursor = new Vector3Int(0, 0, 0);
+        
         for (int s = 0; s < pipeSections.Count - 1; s++)
         {
-            prevSection = section;
-            section = pipeSections[s];
-            nextSection = pipeSections[s + 1];
-            int tMax = t + SectionLength;
-            for (; t < tMax; t++)
-            {
-                int width = GetSectionWidth(section, prevSection, nextSection, t);
-                tilemap.SetTile(centre + Vector3Int.up * width, TopPipeWall);
-                tilemap.SetTile(centre + Vector3Int.down * width, BottomPipeWall);
-                centre.x++;
-                if (section == PipeSectionTypes.MediumGoingUp || section == PipeSectionTypes.SmallGoingUp)
-                {
-                    centre.y++;
-                }
-                else if (section == PipeSectionTypes.MediumGoingDown || section == PipeSectionTypes.SmallGoingDown)
-                {
-                    centre.y--;
-                }
-            }
+            PipeSection pipeSection = pipeSections[s];
+            TileBase[] tiles = pipeSection.Tilemap.GetTilesBlock(pipeSection.SectionSize);
+
+            BoundsInt writePos = new BoundsInt(cursor, pipeSection.SectionSize.size);
+            tilemap.SetTilesBlock(writePos, tiles);
+            cursor += Vector3Int.right*pipeSection.SectionSize.size.x + Vector3Int.up * pipeSection.VerticalOffset;
         }
     }
 
     private void FillRuleDictionary()
     {
-        ruleDictionary = new Dictionary<PipeSectionTypes, Rule>();
-        for (int r = 0; r < Rules.Length; r++)
+        sectionDictionary = new Dictionary<PipeSection.JointType, List<PipeSection>>();
+        for (int p = 0; p < SectionPrefabs.Length; p++)
         {
-            ruleDictionary[Rules[r].From] = Rules[r];
+            PipeSection section = SectionPrefabs[p];
+            if (!sectionDictionary.ContainsKey(section.StartJoint))
+            {
+                sectionDictionary[section.StartJoint] = new List<PipeSection>();
+            }
+            sectionDictionary[section.StartJoint].Add(section);
         }
     }
-
-    int GetSectionWidth(PipeSectionTypes section, PipeSectionTypes prevSection = PipeSectionTypes.Medium, PipeSectionTypes nextSection = PipeSectionTypes.Medium, int t = 0) {
-        switch (section) {
-            case PipeSectionTypes.Small:
-            case PipeSectionTypes.SmallGoingDown:
-            case PipeSectionTypes.SmallGoingUp:
-                return 8;
-            case PipeSectionTypes.Medium:
-            case PipeSectionTypes.MediumGoingDown:
-            case PipeSectionTypes.MediumGoingUp:
-            case PipeSectionTypes.Split:
-                return 16;
-            case PipeSectionTypes.Big:
-                return 24;
-            case PipeSectionTypes.Transition:
-                return GetTransitionSectionWidth(prevSection, nextSection, t);
-            default:
-                return 16;
-        }
-    }
-
-    private int GetTransitionSectionWidth(PipeSectionTypes prevSection, PipeSectionTypes nextSection, int t)
-    {
-        int fromWidth = GetSectionWidth(prevSection);
-        int toWidth = GetSectionWidth(nextSection);
-        float theta = (float)(t%SectionLength) / SectionLength;
-        return Mathf.FloorToInt(Mathf.Lerp(fromWidth, toWidth, theta));
-    }
+    
 }
---
+++ Assets/Scripts/PipeSection.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[RequireComponent(typeof(Tilemap))]
+public class PipeSection : MonoBehaviour
+{
+    public enum JointType
+    {
+        Small,
+        Medium,
+        Big
+    }
+
+    public JointType StartJoint, EndJoint;
+    private Tilemap tilemap;
+
+    public BoundsInt SectionSize { get { return tilemap.cellBounds; } }
+    public int VerticalOffset = 0;
+
+    public Tilemap Tilemap { get { if (tilemap == null) { tilemap = GetComponent<Tilemap>(); } return tilemap; } }
+}
