cf50183346f2cb4fa3755519f49ad2bbf607516f
Izzywizz <isfandyar.ali@me.com>
2019-01-26 13:47:46 +0000

    Able to save and load data using playeholder buttons

--- Assets/Scripts/HighScores.cs
+++ Assets/Scripts/HighScores.cs
@@ -2,26 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class HighScores : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private string gameDataFileName = "save.json";
+    private Player player;
+
+    private void Awake()
     {
-        
+        player = new Player("Isfand", 3242);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    //PlayerPrefs --  can be used to save the player score
     public void GoBack()
     {
         Debug.Log("Go backing to MENU");
         SceneManager.LoadScene("Menu");
 
+        //Saving the player score
+        Debug.Log("TEST: " + player.Name + " score " + player.Score);
+        SavePlayerData();
     }
+
+    public void LoadPlayerSaveData()
+    {
+        string path = Application.dataPath + "/SaveData/save.json";
+
+        if (File.Exists(path))
+        {
+            // Read the json from the file into a string
+            string dataAsJson = File.ReadAllText(path);
+            // Pass the json to JsonUtility, and tell it to create a GameData object from it
+            Player playerLoaded = JsonUtility.FromJson<Player>(dataAsJson);
+            // Retrieve spefic loadedData
+            Debug.Log("LOADED: " + playerLoaded.Name + " score " + playerLoaded.Score);
+
+            //allRoundData = loadedData.allRoundData;
+        }
+        else
+        {
+            Debug.LogError("Cannot load game data!");
+        }
+    }
+
+    public void SavePlayerData()
+    {
+
+        Debug.Log("TEST: " + player.Name + " score " + player.Score);
+
+        string path = Application.dataPath + "/SaveData/save.json";
+        string dataAsJson = JsonUtility.ToJson(player);
+
+        //string filePath = Application.dataPath + path;
+        File.WriteAllText(path, dataAsJson);
+    }
+
+
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Collections;
+
+
+
+[Serializable]
+public class Player
+{
+    public string Name;
+    public int Score;
+    [SerializeField]
+    private int score
+    {
+        get
+        {
+            return Score;
+        }
+        set
+        {
+            Score = value;
+        }
+    }
+    [SerializeField]
+    private string name
+    {
+        get
+        {
+            return Name;
+        }
+        set
+        {
+            Name = value;
+        }
+    }
+
+    public Player(string aName, int aScore)
+    {
+        Name = aName;
+        Score = aScore;
+    }
+
+    public string LoadSaveFromJSON()
+    {
+        Debug.Log("Error downloading from web FAILED");
+        string path = Application.dataPath + "/SaveData/save.json";
+        string jsonString = File.ReadAllText(path);
+
+        return jsonString;
+
+    }
+
+
+}
