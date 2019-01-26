97b25882b569046bedb0bcd22fdc13b81c669e0a
Izzywizz <isfandyar.ali@me.com>
2019-01-26 16:33:12 +0000

    Player name entry scene added

---
+++ Assets/Scripts/NameEntry.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NameEntry : MonoBehaviour
+{
+
+    public InputField input;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        input.onEndEdit.AddListener(ListenToSubmit);
+    }
+
+
+    public void ListenToSubmit(string playerName)
+    {
+        Debug.Log("TEST: " + playerName);
+    }
+
+    public void save()
+    {
+        Debug.Log("TEST: " + input.text);
+        Game.Instance.player.Name = input.text;
+    }
+
+
+}
