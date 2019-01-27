880932a0df4b24c9be4da0d5349ad562ea186297
Joe Bain <mrjoebain@gmail.com>
2019-01-27 11:47:26 +0000

    speed up fish, add boost and bubbles

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,42 +6,27 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D body;
-    public Vector2 UP;
-    public Vector2 DOWN;
-    public Vector2 LEFT;
-    public Vector2 RIGHT;
 
     public Vector2 BOOST;
-    public float FORCE_APPLIED;
-    public float TOP_SPEED;
-    public bool KEY_HOLD_FLAG;
-    public bool KEY_TAP_FLAG;
-    public bool FORWARD_BOOST_FLAG;   
+    public float FORCE_APPLIED = 4f;
+    public float TOP_SPEED = 1f;
+    public float BOOST_FORCE = 10f;
+    public bool FORWARD_BOOST_FLAG;
+    public float BOOST_DURATION = 0.3f;
+    public float BOOST_COOLDOWN = 2f;
     Vector2 velocity;
     Vector2 relative_point;
     float mag;
+    private float boostTime;
+
+    public ParticleSystem bubbleTrail;
 
     // Start is called before the first frame update
     void Start()
     {
-        // set this so the player can hold a key rather than continually press
-        KEY_HOLD_FLAG = true;
-        KEY_TAP_FLAG = false;
-        FORWARD_BOOST_FLAG = true;
-
-        TOP_SPEED = 4.0f;
-        FORCE_APPLIED = 1.0f;
-
         relative_point = new Vector2(0.0f, 0.0f);
 
         body = GetComponent<Rigidbody2D>();
-        UP = new Vector2(0.0f, FORCE_APPLIED);
-        DOWN = new Vector2(0.0f, -FORCE_APPLIED);
-        LEFT = new Vector2(-FORCE_APPLIED, 0.0f);
-        RIGHT = new Vector2(FORCE_APPLIED, 0.0f);
-        /* TODO: change boost so it adds a force in the direction that Fish is headed. 
-        This is a requirement if fish rotates */
-        BOOST = new Vector2(5.0f, 0.0f);
     }
 
     // Update is called once per frame
@@ -50,55 +35,43 @@ public class PlayerMovement : MonoBehaviour
         velocity = body.GetRelativePointVelocity(relative_point);
         mag = velocity.magnitude;
 
-        if (KEY_HOLD_FLAG)
+        float topSpeed = FORWARD_BOOST_FLAG ? TOP_SPEED * 2f : TOP_SPEED;
+        if (mag > topSpeed)
         {
-            if (mag > TOP_SPEED)
-            {
-                body.velocity = velocity.normalized * TOP_SPEED;
-            }
-            if (Input.GetKey(KeyCode.W))
-            {
-                body.AddForce(UP);
-            }
-            if (Input.GetKey(KeyCode.A))
-            {
-                body.AddForce(LEFT);
-            }
-            if (Input.GetKey(KeyCode.S))
-            {
-                body.AddForce(DOWN);
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                body.AddForce(RIGHT);
-            }
-            if (Input.GetKeyDown(KeyCode.Space) && FORWARD_BOOST_FLAG)
-            {
-                body.AddForce(BOOST);
-            }
+            body.velocity = velocity.normalized * topSpeed;
         }
-        else if (KEY_TAP_FLAG && mag < 4)
+        if (Input.GetKey(KeyCode.W))
         {
-            if (Input.GetKeyDown(KeyCode.W))
-            {
-                body.AddForce(UP);
-            }
-            if (Input.GetKeyDown(KeyCode.A))
-            {
-                body.AddForce(LEFT);
-            }
-            if (Input.GetKeyDown(KeyCode.S))
-            {
-                body.AddForce(DOWN);
-            }
-            if (Input.GetKeyDown(KeyCode.D))
-            {
-                body.AddForce(RIGHT);
-            }
-            if (Input.GetKeyDown(KeyCode.Space) && FORWARD_BOOST_FLAG)
-            {
-                body.AddForce(BOOST);
-            }
+            body.AddForce(Vector2.up*FORCE_APPLIED);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            body.AddForce(Vector2.left*FORCE_APPLIED);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            body.AddForce(Vector2.down*FORCE_APPLIED);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            body.AddForce(Vector2.right*FORCE_APPLIED);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time - boostTime > BOOST_COOLDOWN)
+        {
+            boostTime = Time.time;
+        }
+        if (Input.GetKey(KeyCode.Space) && Time.time - boostTime < BOOST_DURATION)
+        {
+            body.AddForce(Vector2.right * BOOST_FORCE);
+            FORWARD_BOOST_FLAG = true;
+            var emission = bubbleTrail.emission;
+            emission.enabled = true;
+        }
+        else
+        {
+            FORWARD_BOOST_FLAG = false;
+            var emission = bubbleTrail.emission;
+            emission.enabled = false;
         }
     }
 }
